8940283067b877d466d19e1a9e7c72b999da2282
ProbablyCreative <85829@studmail.htw-aalen.de>
2023-12-28 20:18:01 +0100

    Close to respawning darts

--- Unity/Bloons/Assets/Skripts/SpawnManager/SpawnDarts.cs
+++ Unity/Bloons/Assets/Skripts/SpawnManager/SpawnDarts.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class SpawnDarts : MonoBehaviour
 {
+    [SerializeField] public GameObject[] darts;
+    [SerializeField] public Vector3[] position;
+    [SerializeField] public Vector3[] rotaion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (darts.Length != position.Length || darts.Length != rotaion.Length) //TODO entfernen, only debug
+        {
+            Debug.Log("Error ->" +  darts.Length + "Darts and " + position.Length + "Positions" );
+            return;
+        }
+        for (int i = 0; i < darts.Length; i++)
+        {
+            darts[i].GetComponent<dart>().dartNum = i;
+            Instantiate(darts[i], position[i], Quaternion.Euler(rotaion[i]));
+        }
+    }
+
+    public void replaceDart(int d)
+    {
+        Instantiate(darts[d], position[d], Quaternion.Euler(rotaion[d]));
     }
 
     // Update is called once per frame
