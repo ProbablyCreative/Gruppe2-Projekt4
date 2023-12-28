292c79e1ba73b1980a511508841100628517e778
ProbablyCreative <85829@studmail.htw-aalen.de>
2023-12-28 21:32:27 +0100

    Darts Work

--- Unity/Bloons/Assets/Skripts/Dart/dart.cs
+++ Unity/Bloons/Assets/Skripts/Dart/dart.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class dart : MonoBehaviour
 {
-    public int dartNum;
-    public SpawnDarts spawnDarts;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spawnDarts = GameObject.FindWithTag("SpawnManager").GetComponent<SpawnDarts>();
-    }
-
-    public void replace()
-    {
-        spawnDarts.replaceDart(dartNum);
-    }
+    
 }
--- Unity/Bloons/Assets/Skripts/SpawnManager/SpawnDarts.cs
+++ Unity/Bloons/Assets/Skripts/SpawnManager/SpawnDarts.cs
@@ -4,32 +4,18 @@ using UnityEngine;
 
 public class SpawnDarts : MonoBehaviour
 {
-    [SerializeField] public GameObject[] darts;
-    [SerializeField] public Vector3[] position;
-    [SerializeField] public Vector3[] rotaion;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public GameObject dart;
+    private GameObject current;
+
+    private void Start()
     {
-        if (darts.Length != position.Length || darts.Length != rotaion.Length) //TODO entfernen, only debug
-        {
-            Debug.Log("Error ->" +  darts.Length + "Darts and " + position.Length + "Positions" );
-            return;
-        }
-        for (int i = 0; i < darts.Length; i++)
-        {
-            darts[i].GetComponent<dart>().dartNum = i;
-            Instantiate(darts[i], position[i], Quaternion.Euler(rotaion[i]));
-        }
+        current = Instantiate(dart, transform.position, transform.rotation);
+        current.GetComponent<activateMovement>().spawner = this;
     }
 
-    public void replaceDart(int d)
-    {
-        Instantiate(darts[d], position[d], Quaternion.Euler(rotaion[d]));
+    public void spawnNew() {
+        current = Instantiate(dart, transform.position, transform.rotation);
+        current.GetComponent<activateMovement>().spawner = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
