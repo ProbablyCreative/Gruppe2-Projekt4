ad26c94884b0c0dcc4cf1986663b47c9aabf0065
Flottie <153534473+Flottie@users.noreply.github.com>
2024-01-20 22:29:30 +0100

    Update Geld.cs

--- Unity/Bloons/Assets/Skripts/Geld/Geld.cs
+++ Unity/Bloons/Assets/Skripts/Geld/Geld.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Geld : MonoBehaviour
 {
+    public int gold = 0;
+    public int weaponUpgrade = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,20 @@ public class Geld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        BuyWeaponUpgrade();
+    }
+
+    public void OnBalloonDestroyed()
+    {
+        gold += 5;
+    }
+
+    private void BuyWeaponUpgrade()
+    {
+        if (gold >= 200)
+        {
+            gold -= 200;
+            weaponUpgrade++;
+        }
     }
 }
